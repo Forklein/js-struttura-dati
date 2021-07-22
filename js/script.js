c74d7b35a35144f9f43d9b23272b147fd659ecc5
Forklein <giuseppepisani2@gmail.com>
2021-07-22 15:50:43 +0200

    add style in css and start innerhtml

--- js/script.js
+++ js/script.js
@@ -1,6 +1,60 @@
-console.log('Ciao Peppe!');
-
 const card = {
-    name: "Bloodfire Colossus",
-    launchCost: ["6", "R", "R"],
+    id: 1,
+    name: 'Bloodfire Colossus',
+    launchCost: ['6', 'R', 'R'],
+    combinedManaCost: 8,
+    cardType: 'Creatura',
+    subType: 'Giant',
+    expansion: {
+        reprintId: 9,
+        name: 'Limited Expansion',
+        rarity: 'Gold',
+        collectionNr: 12,
+        totalCard: 42,
+    },
+    flavorText: {
+        quote: 'Description',
+        author: 'Forklein',
+    },
+    abilities: [
+        {
+            launchCost: ['R', 'T'],
+            description: 'Destroy all monster',
+        },
+        {
+            launchCost: ['5', 'R', 'U'],
+            description: 'Increase Strenght'
+        },
+    ],
+    constitution: 13,
+    strength: 13,
+    borderColor: '#000',
+    illustration: {
+        author: {
+            id: 1,
+            name: 'Greg Smith',
+        },
+        source: '..img/pic.jpg',
+    },
+    background: {
+        color: 'red',
+        source: '..img/pic.jpg',
+    }
 }
+
+const containerDisplay = document.getElementById('container');
+
+const cardTemplate = `
+<ul>
+    <li>${card.id}</li>
+    <li>Test</li>
+    <li>Test</li>
+    <li>Test</li>
+    <li>Test</li>
+    <li>Test</li>
+    <li>Test</li>
+    <li>Test</li>
+</ul>`;
+
+containerDisplay.innerHTML = cardTemplate;
+
